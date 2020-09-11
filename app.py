337d7e725d8761f6f1b7a4635dbc9310cde29928
jaikrish.dxj@gmail.com <Sjaikrish.Jaikrish@pb.com>
2020-09-11 17:37:18 +0530

    finally ui is ready to be deployed

--- app.py
+++ app.py
@@ -22,16 +22,45 @@ def no_children_func(number_of_children):
     else:
         return 0
 
+def rate_marriage_mapping(rate_marriage):
+    if rate_marriage == 'very poor':
+        return 1
+    elif rate_marriage == 'poor':
+        return 2
+    elif rate_marriage == 'fair':
+        return 3
+    elif rate_marriage == 'good':
+        return 4
+    else:
+        return 5
+
+def religious_mapping(religious):
+    if religious == 'not':
+        return 1
+    elif religious == 'mildly':
+        return 2
+    elif religious == 'fairly':
+        return 3
+    else:
+        return 4           
+
+
+
 
 @app.route('/math', methods=['POST'])  # This will be called from UI
 def math_operation():
     if (request.method=='POST'):
         #operation=request.form['operation']
-        rate_marriage=float(request.form['num1'])
-        age =float(request.form['num2'])
-        religious =float(request.form['num3'])
-        years_married = int(request.form['num4'])
-        number_of_children = int(request.form['num5'])
+        rate_marriage=(request.form['rate_marriage'])
+        age =float(request.form['age'])
+        religious =(request.form['Religious'])
+        years_married = int(request.form['years_married'])
+        number_of_children = int(request.form['Children'])
+
+        rate_marriage = rate_marriage_mapping(rate_marriage)
+        religious = religious_mapping(religious)
+
+        
 
         new_marriage = new_marriage_func(years_married)
         no_children = no_children_func(number_of_children)
@@ -50,11 +79,11 @@ def math_operation():
         int_features.append(new_marriage)
         int_features.append(no_children)
 
-        print(int_features)
+        
 
         final_features = [np.array(int_features)]
 
-        print(final_features)
+        
 
 
 
@@ -74,24 +103,9 @@ def math_operation():
 
 
 
-        print(prediction)
+        
 
-        #output = num1+num2+num3
-
-
-
-        # if(operation=='add'):
-        #     r=num1+num2
-        #     result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
-        # if (operation == 'subtract'):
-        #     r = num1 - num2
-        #     result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
-        # if (operation == 'multiply'):
-        #     r = num1 * num2
-        #     result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
-        # if (operation == 'divide'):
-        #     r = num1 / num2
-        #     result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        
         return render_template('results.html',result=prediction)  
 
 
